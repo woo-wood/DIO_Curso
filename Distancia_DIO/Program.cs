374f542a48fbddf912b2efd15face28b9ee48f41
woo-wood <woo-wood@users.noreply.github.com>
2020-12-26 19:59:34 -0300

    Distancia

---
+++ Distancia_DIO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Distancia_DIO
+{
+    class Program
+    {
+        static void Main() {
+        //Console.WriteLine("digite a distancia percorrida");
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 60 / 30; // Digite aqui o calculo dos minutos
+        Console.WriteLine(minutos + " minutos");
+    }
+}
+}
