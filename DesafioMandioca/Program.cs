aadee3d01d9acaef243bcd41ec7dd0c09d397af9
woo-wood <woo-wood@users.noreply.github.com>
2020-12-28 21:45:08 -0300

    DesafioMandioca_OK

--- DesafioMandioca/Program.cs
+++ DesafioMandioca/Program.cs
@@ -6,7 +6,18 @@ namespace DesafioMandioca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int chico = 300 * Int32.Parse(Console.ReadLine());
+            int bento = 1500 * Int32.Parse(Console.ReadLine());
+            int bernardo = 600 * Int32.Parse(Console.ReadLine());
+            int marina = 1000 * Int32.Parse(Console.ReadLine());
+            int iara = 150 * Int32.Parse(Console.ReadLine());
+            int marlene = 225;
+            int total = chico+bento+bernardo+marina+iara+marlene; // Digite aqui o calculo total
+            Console.WriteLine(total);
         }
-    }
+
+    }       
+    
+
+   
 }
